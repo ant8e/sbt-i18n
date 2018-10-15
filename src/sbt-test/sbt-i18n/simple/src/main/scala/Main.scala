7212341210c893a56b4ed1bce813015350b80793
Antoine Comte <antoine@comte.cc>
2018-10-15 09:12:54 +0200

    Include subsection generation in scripted test

--- src/sbt-test/sbt-i18n/simple/src/main/scala/Main.scala
+++ src/sbt-test/sbt-i18n/simple/src/main/scala/Main.scala
@@ -7,6 +7,7 @@ class Main {
   val text = tech.ant8e.i18n.Bundle.fr.test
   val text2 = tech.ant8e.i18n.Bundle.fr.test2("hi")
   val text3 = tech.ant8e.i18n.Bundle.fr.test3(new java.util.Date())
+  val text4 = tech.ant8e.i18n.Bundle.fr.sub.test4
 }
 
 object Main {
