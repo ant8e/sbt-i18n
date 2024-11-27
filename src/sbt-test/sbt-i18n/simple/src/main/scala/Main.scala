968714e118bd067daf33973793f6d01ae506341f
Antoine Comte <antoine@comte.cc>
2024-11-27 17:42:37 +0100

    Fix scripted tests

--- src/sbt-test/sbt-i18n/simple/src/main/scala/Main.scala
+++ src/sbt-test/sbt-i18n/simple/src/main/scala/Main.scala
@@ -5,7 +5,7 @@ package simple
 class Main {
   val text  = tech.ant8e.i18n.Bundle.fr.test
   val text2 = tech.ant8e.i18n.Bundle.fr.test2("hi")
-  val text3 = tech.ant8e.i18n.Bundle.fr.test3(new java.util.Date())
+  val text3 = tech.ant8e.i18n.Bundle.fr.test3(java.time.ZonedDateTime.now())
   val text4 = tech.ant8e.i18n.Bundle.fr.sub.test4
 }
 
